dab270799edc6ba8ac3d43c8089c9511fe98e2d9
MrYurihi <mryurihi2003@gmail.com>
2018-03-27 06:25:34 -0400

    Merge part I

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,11 @@ import { CommandError } from "./brigadier_components/errors";
 import { StringReader } from "./brigadier_components/string_reader";
 import { Resources } from "./data/datapack_resources";
 import { CommandNodePath, GlobalData } from "./data/types";
+<<<<<<< HEAD
+=======
+import { HighlightScope } from "./highlight/highlight_util";
+import { ContextInformation } from "./parse/context";
+>>>>>>> afb6f75... Work on command context
 
 /**
  * A deeply readonly version of the given type.
@@ -88,12 +93,12 @@ export interface Parser {
      * Parse the argument as described in NodeProperties against this parser in the reader.
      * The context is optional for tests
      */
-    parse: (reader: StringReader, properties: ParserInfo) => ParseResult | void;
+    parse: (reader: StringReader, properties: ParserInfo, context?: ContextInformation) => ParseResult | void;
     /**
      * List the suggestions at the end of the starting text described in `text`.
      * @returns an array of Suggestions, either strings or a Suggestion objection
      */
-    getSuggestions: (text: string, context: ParserInfo) => SuggestResult[];
+    getSuggestions: (text: string, properties: ParserInfo, context?: ContextInformation) => SuggestResult[];
     /**
      * The kind of the suggestion in the Completion List
      */
@@ -106,7 +111,7 @@ export interface ParseResult {
      */
     successful: boolean;
     /**
-     * The error is parsing was not successful.
+     * The error if parsing was not successful.
      */
     errors?: CommandError[];
     actions?: SubAction[];
