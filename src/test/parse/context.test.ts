cb042e7f26656f1eeea0c337fa962733def7b27f
MrYurihi <mryurihi2003@gmail.com>
2018-03-29 06:58:19 -0400

    Add context tests

---
+++ src/test/parse/context.test.ts
@@ -0,0 +1,12 @@
+import { Parser } from "../../types";
+
+// @ts-ignore
+const parser: Parser = {
+    parse: () => undefined,
+
+    getSuggestions: () => [],
+};
+
+describe("Context testing", () => {
+    //
+});
