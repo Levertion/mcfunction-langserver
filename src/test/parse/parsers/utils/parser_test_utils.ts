f53cc755b8a9ad32dccebc6002a53ab381898aaa
Levertion <26185209+Levertion@users.noreply.github.com>
2018-04-07 18:19:44 +0100

    Add suggestion and allow undefined in error asserters

--- src/test/parse/parsers/utils/parser_test_utils.ts
+++ src/test/parse/parsers/utils/parser_test_utils.ts
@@ -1,7 +1,8 @@
 import { AssertionError } from "assert";
 import { CommandError, isCommandError } from "../../../../brigadier_components/errors";
+import { SuggestResult } from "../../../../types";
 
-interface ErrorInfo {
+export interface ErrorInfo {
     range: {
         start: number,
         end: number,
@@ -9,27 +10,31 @@ interface ErrorInfo {
     code: string;
 }
 
-export function assertErrors(expected: ErrorInfo[], realErrors: CommandError[]) {
-    if (expected.length > realErrors.length) {
-        throw new AssertionError({
-            message: `Too many errors were given. Actual errors were: '${JSON.stringify(realErrors)}'`,
-        });
-    }
-    if (expected.length < realErrors.length) {
-        throw new AssertionError({
-            message: `Too few errors were given. Actual errors were: '${JSON.stringify(realErrors)}'`,
-        });
-    }
-    for (const info of expected) {
-        const index = realErrors.findIndex((v) => errorMatches(info, v));
-        if (index === -1) {
+export function assertErrors(expected: ErrorInfo[], realErrors: CommandError[] | undefined) {
+    if (!!realErrors) {
+        if (expected.length < realErrors.length) {
             throw new AssertionError({
-                message: `Expected errors to contain an error with code '${info.code}' and range ${
-                    info.range.start}...${info.range.end}, but none met this. The errors were ${
-                    JSON.stringify(realErrors)}`,
+                message: `Too many errors were given. Actual errors were: '${JSON.stringify(realErrors)}'`,
             });
         }
-        realErrors.splice(index, 1);
+        if (expected.length > realErrors.length) {
+            throw new AssertionError({
+                message: `Too few errors were given. Actual errors were: '${JSON.stringify(realErrors)}'`,
+            });
+        }
+        for (const info of expected) {
+            const index = realErrors.findIndex((v) => errorMatches(info, v));
+            if (index === -1) {
+                throw new AssertionError({
+                    message: `Expected errors to contain an error with code '${info.code}' and range ${
+                        info.range.start}...${info.range.end}, but none met this. The errors were ${
+                        JSON.stringify(realErrors)}`,
+                });
+            }
+            realErrors.splice(index, 1);
+        }
+    } else if (expected.length > 0) {
+        throw new AssertionError({ message: "Expected an array of errors, got none" });
     }
 }
 
@@ -50,6 +55,39 @@ export function thrownErrorAssertion(expected: ErrorInfo): (error: any) => true 
             });
         }
     };
+}
+
+export function assertSuggestions(expected: string[], start: number, actual: SuggestResult[] | undefined) {
+    if (!!actual) {
+        if (expected.length > actual.length) {
+            throw new AssertionError({
+                message: `Too many suggestions were given. Actual suggestions were: '${JSON.stringify(actual)}'`,
+            });
+        }
+        if (expected.length < actual.length) {
+            throw new AssertionError({
+                message: `Too few suggestions were given. Actual suggestions were: '${JSON.stringify(actual)}'`,
+            });
+        }
+        for (const expectation of expected) {
+            const index = actual.findIndex((v) => {
+                if (typeof v === "string") {
+                    return start === 0 && v === expectation;
+                } else {
+                    return v.value === expectation && v.start === start;
+                }
+            });
+            if (index === -1) {
+                throw new AssertionError({
+                    message: `Expected suggestions to contain a suggestion starting with '${expectation
+                        }', but got ${JSON.stringify(actual)}`,
+                });
+            }
+            actual.splice(index, 1);
+        }
+    } else if (expected.length > 0) {
+        throw new AssertionError({ message: "Expected an array of suggestions, got none" });
+    }
 }
 
 function errorMatches(expected: ErrorInfo, error: CommandError): boolean {
