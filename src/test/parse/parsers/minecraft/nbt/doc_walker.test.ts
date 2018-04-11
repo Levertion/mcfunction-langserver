8a594416862d8463574e67580da73c2c80388fac
MrYurihi Redstone <mryurihi2003@gmail.com>
2018-04-11 08:42:43 -0400

    Change around & finish tests for doc walker

--- src/test/parse/parsers/minecraft/nbt/doc_walker.test.ts
+++ src/test/parse/parsers/minecraft/nbt/doc_walker.test.ts
@@ -1,46 +1,72 @@
 import assert = require("assert");
 import path = require("path");
-import { NBTWalker, NoPropertyNode } from "../../../../../parse/parsers/minecraft/nbt/doc_walker";
+import { NBTWalker } from "../../../../../parse/parsers/minecraft/nbt/doc_walker";
 import { NBTTagCompound } from "../../../../../parse/parsers/minecraft/nbt/tag/compound_tag";
 
 describe("Documentation Walker Tests", () => {
     describe("getFinalNode()", () => {
         const nbt = new NBTTagCompound({});
+        const walker = new NBTWalker(nbt, path.resolve(__dirname,
+            "../../../../../../test_data/test_docs/root.json"));
 
-        it("should return the correct node for the basic NBT Docs", () => {
-            const walker = new NBTWalker(nbt, path.resolve(__dirname,
-                "../../../../../../test_data/test_docs/test1.json"));
-            const node = walker.getFinalNode(["key1", "key2"]);
+        it("should return the correct node for the basic doc", () => {
+            const node = walker.getFinalNode(["basic_test"]);
             if (!node) {
                 assert.fail("node is undefined");
                 return;
             }
-            assert.ok("type" in node);
-            assert.ok((node as NoPropertyNode).type === "string", "node type was " + (node as NoPropertyNode).type);
+            assert.ok("description" in node);
+            assert.ok(node.description === "basic_test OK");
         });
 
-        it("should return the correct node for the child_ref property", () => {
-            const walker = new NBTWalker(nbt, path.resolve(__dirname,
-                "../../../../../../test_data/test_docs/child_ref_test/test2.json"));
-            const node = walker.getFinalNode(["key1"]);
+        it("should return the correct node for nested nodes", () => {
+            const node = walker.getFinalNode(["nest_test", "key0"]);
             if (!node) {
                 assert.fail("node is undefined");
                 return;
             }
-            assert.ok("type" in node);
-            assert.ok((node as NoPropertyNode).type === "int", "node type was " + (node as NoPropertyNode).type);
+            assert.ok("description" in node);
+            assert.ok(node.description === "nest_test OK");
         });
 
         it("should return the correct node for the ref property", () => {
-            const walker = new NBTWalker(nbt, path.resolve(__dirname,
-                "../../../../../../test_data/test_docs/ref_test/test3.json"));
-            const node = walker.getFinalNode(["key1"]);
+            const node = walker.getFinalNode(["ref_test"]);
             if (!node) {
                 assert.fail("node is undefined");
                 return;
             }
-            assert.ok("type" in node);
-            assert.ok((node as NoPropertyNode).type === "short", "node type was " + (node as NoPropertyNode).type);
+            assert.ok("description" in node);
+            assert.ok(node.description === "ref_test OK");
+        });
+
+        it("should return the correct node for the child_ref property", () => {
+            const node = walker.getFinalNode(["child_ref_test", "key0"]);
+            if (!node) {
+                assert.fail("node is undefined");
+                return;
+            }
+            assert.ok("description" in node);
+            assert.ok(node.description === "child_ref_test OK");
+        });
+
+        it("should return the correct node for the child_ref property but self", () => {
+            const node = walker.getFinalNode(["child_ref_test", "key1"]);
+            if (!node) {
+                assert.fail("node is undefined");
+                return;
+            }
+            assert.ok("description" in node);
+            assert.ok(node.description === "child_ref_self_test OK");
+        });
+
+        it("should return the correct node for lists", () => {
+            const node = walker.getFinalNode(["list_test", "0"]);
+            if (!node) {
+                assert.fail("node is undefined");
+                return;
+            }
+            assert.ok("description" in node);
+            assert.ok(node.description === "list_test OK");
         });
     });
 });
