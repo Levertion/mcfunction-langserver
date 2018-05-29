989c9132e66eaf29ef6b08ff75def60ef9511a8c
MrYurihi <mryurihi2003@gmail.com>
2018-05-29 16:26:40 -0400

    fix reviews

--- src/misc_functions/context.ts
+++ src/misc_functions/context.ts
@@ -1,18 +1,18 @@
 export interface ContextPath<T> {
     path: string[];
-    data: (args: string[]) => T;
+    data: T;
 }
 
-export function resolvePaths<T>(paths: Array<ContextPath<T>>, argPath: string[], args: string[]): T | undefined {
+export function resolvePaths<T>(paths: Array<ContextPath<T>>, argPath: string[]): T | undefined {
     for (const path of paths) {
-        if (arreq(argPath, path.path)) {
-            return path.data(args);
+        if (stringArrayEqual(argPath, path.path)) {
+            return path.data;
         }
     }
     return undefined;
 }
 
-function arreq(arr1: string[], arr2: string[]) {
+function stringArrayEqual(arr1: string[], arr2: string[]) {
     return arr1.length === arr2.length &&
         arr1.every(
             (v, i) => v === arr2[i],
