269b27ded0edb6084f84450662ad36cc6b869ecd
Levertion <26185209+Levertion@users.noreply.github.com>
2018-07-23 21:36:06 +0100

    Allow tags in predicates

--- src/parsers/minecraft/block/predicate.ts
+++ src/parsers/minecraft/block/predicate.ts
@@ -2,7 +2,7 @@ import { Parser } from "../../../types";
 import { parseBlockArgument } from "./shared";
 
 const parser: Parser = {
-    parse: (reader, info) => parseBlockArgument(reader, info, false)
+    parse: (reader, info) => parseBlockArgument(reader, info, true)
 };
 
 export = parser;
