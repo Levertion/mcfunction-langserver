38e61b8a5e7e95bcfdde6371bde9cae0b0088d88
Levertion <26185209+Levertion@users.noreply.github.com>
2018-09-12 21:45:13 +0100

    Add general resource parsers

--- src/parsers/minecraft/resources.ts
+++ src/parsers/minecraft/resources.ts
@@ -1,18 +1,40 @@
 import { CompletionItemKind } from "vscode-languageserver";
 import { CommandErrorBuilder } from "../../brigadier/errors";
+import { Resources } from "../../data/types";
 import {
+    ContextPath,
+    convertToNamespace,
     getResourcesofType,
+    parseNamespace,
+    parseNamespaceOption,
     parseNamespaceOrTag,
     processParsedNamespaceOption,
     ReturnHelper,
+    startPaths,
     stringifyNamespace
 } from "../../misc-functions";
 import { Parser } from "../../types";
 
 const exceptions = {
+    advancement_notfound: new CommandErrorBuilder(
+        "advancement.advancementNotFound",
+        "Unknown advancement: %s"
+    ),
+    nobossbar: new CommandErrorBuilder(
+        "",
+        "No bossbar exists with the ID '%s'"
+    ),
+    recipe_notfound: new CommandErrorBuilder(
+        "recipe.notFound",
+        "Unkown recipe: %s"
+    ),
     unknown_function: new CommandErrorBuilder(
         "arguments.function.unknown",
         "Unknown function '%s'"
+    ),
+    unknown_resource: new CommandErrorBuilder(
+        "argument.id.unknown",
+        "Unknown resource '%s'"
     ),
     unknown_tag: new CommandErrorBuilder(
         "arguments.function.tag.unknown",
@@ -61,6 +83,113 @@ export const functionParser: Parser = {
                     )
                 );
             }
+        }
+    }
+};
+
+const idParser: Parser = {
+    parse: parseNamespace
+};
+
+const bossbarParser: Parser = {
+    parse: (reader, info) => {
+        if (info.data.localData && info.data.localData.nbt.level) {
+            const start = reader.cursor;
+            const helper = new ReturnHelper();
+            const bars = info.data.localData.nbt.level.Data.CustomBossEvents;
+            const options = Object.keys(bars).map((v, _) =>
+                convertToNamespace(v)
+            );
+            const result = parseNamespaceOption(reader, options);
+            if (helper.merge(result)) {
+                return helper.succeed();
+            } else {
+                if (result.data) {
+                    return helper.fail(
+                        exceptions.nobossbar.create(
+                            start,
+                            reader.cursor,
+                            stringifyNamespace(result.data)
+                        )
+                    );
+                } else {
+                    return helper.fail();
+                }
+            }
+        } else {
+            return parseNamespace(reader);
+        }
+    }
+};
+
+const resourceKinds: Array<
+    ContextPath<
+        | {
+              issue: CommandErrorBuilder;
+              resource: keyof Resources;
+          }
+        | { resource: Parser }
+    >
+> = [
+    {
+        data: {
+            issue: exceptions.advancement_notfound,
+            resource: "advancements"
+        },
+        path: ["advancement"]
+    },
+    { data: { resource: idParser }, path: ["bossbar", "add"] },
+    { data: { resource: bossbarParser }, path: ["bossbar"] },
+    {
+        data: { resource: bossbarParser },
+        path: ["execute", "store", "result"]
+    },
+    // Worrying about sounds is not in scope for initial release
+    { data: { resource: idParser }, path: ["playsound"] },
+    { data: { resource: idParser }, path: ["stopsound"] },
+    {
+        data: { resource: "recipes", issue: exceptions.advancement_notfound },
+        path: ["recipe"]
+    }
+];
+
+export const resourceParser: Parser = {
+    parse: (reader, info) => {
+        const start = reader.cursor;
+        const helper = new ReturnHelper(info);
+        const kind = startPaths(resourceKinds, info.path);
+        if (kind) {
+            if (typeof kind.resource === "object") {
+                return kind.resource.parse(reader, info);
+            } else {
+                const result = parseNamespaceOption(
+                    reader,
+                    getResourcesofType(info.data, kind.resource)
+                );
+                if (helper.merge(result)) {
+                    return helper.succeed();
+                } else {
+                    if (result.data) {
+                        helper.addErrors(
+                            // @ts-ignore type inference failure
+                            (kind.issue as CommandErrorBuilder).create(
+                                start,
+                                reader.cursor,
+                                stringifyNamespace(result.data)
+                            )
+                        );
+                        return helper.succeed();
+                    } else {
+                        return helper.fail();
+                    }
+                }
+            }
+        } else {
+            throw new Error(
+                `Resource at path ${
+                    info.path
+                } does not have a supported resource`
+            );
         }
     }
 };
