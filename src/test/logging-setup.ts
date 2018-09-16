c24be34874a8f962cf666353acec14c3684644b7
Levertion <26185209+Levertion@users.noreply.github.com>
2018-09-16 11:00:27 +0100

    Change to using parcel and ts-node

--- src/test/logging-setup.ts
+++ src/test/logging-setup.ts
@@ -1,11 +1,4 @@
-import { dummyParser } from "./parsers/tests/dummy1";
-
 function setup_logging(): void {
-    global.mcLangSettings = ({
-        parsers: {
-            "langserver:dummy1": dummyParser
-        }
-    } as any) as McFunctionSettings;
     const logger = (message: string) => {
         // tslint:disable-next-line:no-console
         console.log(message);
