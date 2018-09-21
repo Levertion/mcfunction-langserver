251f6ff75b60f360e2eb0030e1ab683e9f64a4b1
Levertion <26185209+Levertion@users.noreply.github.com>
2018-09-21 19:47:28 +0100

    Use import helpers and fix startPaths (#73)

--- src/test/misc-functions/context.test.ts
+++ src/test/misc-functions/context.test.ts
@@ -1,21 +1,64 @@
 import * as assert from "assert";
-import { ContextPath, resolvePaths } from "../../misc-functions/context";
+import {
+    ContextPath,
+    resolvePaths,
+    startPaths
+} from "../../misc-functions/context";
 
 describe("context tests", () => {
+    type pth = ContextPath<string>;
+    const options: pth[] = [
+        { data: "yay", path: ["foo1", "foo2", "foo3"] },
+        { data: "longer", path: ["1", "2", "3", "4"] },
+        { data: "shorter", path: ["1"] }
+    ];
     describe("resolvePaths()", () => {
-        type pth = ContextPath<string>;
-        const arr: pth[] = [
-            { data: "yay", path: ["foo1", "foo2", "foo3"] },
-            { data: "fail", path: ["x", "k", "c", "d"] }
-        ];
         it("should return the correct paths", () => {
             assert.strictEqual(
-                resolvePaths(arr, ["foo1", "foo2", "foo3"]),
+                resolvePaths(options, ["foo1", "foo2", "foo3"]),
                 "yay"
             );
         });
         it("should return undefined if it cannot find the path", () => {
-            assert.strictEqual(resolvePaths(arr, ["blip", "blop"]), undefined);
+            assert.strictEqual(
+                resolvePaths(options, ["blip", "blop"]),
+                undefined
+            );
+        });
+    });
+    describe("startPaths", () => {
+        it("should return the correct value when an exact match is found", () => {
+            assert.strictEqual(
+                startPaths(options, ["foo1", "foo2", "foo3"]),
+                "yay"
+            );
+        });
+        it("should return undefined if it cannot find the path", () => {
+            assert.strictEqual(
+                startPaths(options, ["blip", "blop"]),
+                undefined
+            );
+        });
+        it("should allow an extended path", () => {
+            assert.strictEqual(
+                startPaths(options, ["foo1", "foo2", "foo3", "foo4"]),
+                "yay"
+            );
+        });
+        it("should give a longer path than a shorter path", () => {
+            assert.strictEqual(
+                startPaths(options, ["1", "2", "3", "4"]),
+                "longer"
+            );
+        });
+        it("should give a shorter path for part of a longer", () => {
+            assert.strictEqual(startPaths(options, ["1", "2", "3"]), "shorter");
+        });
+        it("should give a longer path when there are too many characters", () => {
+            assert.strictEqual(
+                startPaths(options, ["1", "2", "3", "4", "5"]),
+                "longer"
+            );
         });
     });
 });
