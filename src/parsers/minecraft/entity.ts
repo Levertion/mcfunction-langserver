013b3ba84a9926d6958e0a5087004fd7e17401b1
MrYurihi <mryurihi2003@gmail.com>
2018-11-08 14:58:58 -0500

    start entity parser

---
+++ src/parsers/minecraft/entity.ts
@@ -0,0 +1,253 @@
+import { DiagnosticSeverity } from "vscode-languageserver";
+import { CommandErrorBuilder } from "../../brigadier/errors";
+import { StringReader } from "../../brigadier/string-reader";
+import { entities } from "../../data/lists/statics";
+import {
+    convertToNamespace,
+    namespacesEqual,
+    parseNamespaceOrTag,
+    ReturnHelper,
+    stringArrayEqual
+} from "../../misc-functions";
+import { ContextChange, Parser, ParserInfo, ReturnedInfo } from "../../types";
+import { nbtParser } from "./nbt/nbt";
+
+const uuidregex = /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/g;
+
+/*https://github.com/Levertion/mcfunction-langserver/issues/89*/
+const uuidwarn = new CommandErrorBuilder(
+    "argument.entity.uuid",
+    "Selecting an entity based on its UUID may cause instability [This warning can be disabled in the settings]",
+    DiagnosticSeverity.Warning
+);
+
+interface NodeProp {
+    amount: "single" | "multiple";
+    type: "entity" | "player";
+}
+
+interface EntityContext {
+    limit?: number;
+    tags?: string[];
+    team?: string;
+    type?: string[];
+}
+
+const contexterr = {
+    oneEntity: new CommandErrorBuilder(
+        "argument.entity.toomany",
+        "Only one entity is allowed, but the provided selector allows more than one"
+    ),
+    onePlayer: new CommandErrorBuilder(
+        "argument.player.toomany",
+        "Only one player is allowed, but the provided selector allows more than one"
+    ),
+    onlyPlayer: new CommandErrorBuilder(
+        "argument.player.entities",
+        "Only players may be affected by this command, but the provided selector includes entities"
+    )
+};
+
+const getContextError = (cont: EntityContext, prop: NodeProp) => {
+    if (
+        prop.type === "player" &&
+        stringArrayEqual(cont.type || [], ["minecraft:player"])
+    ) {
+        return contexterr.onlyPlayer;
+    }
+    if (prop.amount === "single" && cont.limit !== 1) {
+        return prop.type === "entity"
+            ? contexterr.oneEntity
+            : contexterr.onePlayer;
+    }
+    return undefined;
+};
+
+interface OptionParser {
+    allowMultiple: boolean;
+    parse(
+        reader: StringReader,
+        info: ParserInfo,
+        context: EntityContext
+    ): ReturnedInfo<EntityContext | undefined>;
+}
+
+// tslint:disable-next-line:no-unnecessary-callback-wrapper it gives an error if it isn't wrapped
+const nsEntity = entities.map(v => convertToNamespace(v));
+
+const options: { [key: string]: OptionParser } = {
+    nbt: {
+        allowMultiple: true,
+        parse: (reader, info) => {
+            const helper = new ReturnHelper();
+            const res = nbtParser.parse(reader, info);
+            if (!helper.merge(res)) {
+                return helper.fail();
+            } else {
+                return helper.succeed();
+            }
+        }
+    },
+    type: {
+        allowMultiple: true,
+        parse: (reader, info, context) => {
+            const helper = new ReturnHelper();
+            const negated = reader.peek() === "!";
+            if (negated) {
+                reader.skip();
+            }
+            const ltype = parseNamespaceOrTag(reader, info, "entity_tags");
+            if (!helper.merge(ltype)) {
+                return helper.fail();
+            }
+
+            const types = nsEntity.filter(
+                v =>
+                    negated ===
+                    !(ltype.data.resolved || [ltype.data.parsed]).find(f =>
+                        namespacesEqual(f, v)
+                    )
+            );
+
+            const intTypes: string[] = [];
+
+            if (context.type) {
+                for (const type of context.type) {
+                    if (types.find(v => v === convertToNamespace(type))) {
+                        intTypes.push(type);
+                    }
+                }
+            } else {
+                types.forEach(v =>
+                    intTypes.push(`${v.namespace || "minecraft"}:${v.path}`)
+                );
+            }
+
+            if (intTypes.length === 0) {
+                return helper.fail();
+            } else {
+                return helper.succeed({
+                    type: intTypes
+                } as EntityContext);
+            }
+        }
+    }
+};
+
+export class EntityBase implements Parser {
+    private readonly fakePlayer: boolean;
+
+    public constructor(fakePlayer: boolean) {
+        this.fakePlayer = fakePlayer;
+    }
+
+    // tslint:disable:cyclomatic-complexity
+    public parse(
+        reader: StringReader,
+        info: ParserInfo
+    ): ReturnedInfo<ContextChange> {
+        const helper = new ReturnHelper();
+        if (
+            helper.merge(reader.expect("@"), {
+                errors: false
+            })
+        ) {
+            const start = reader.cursor;
+            const context: EntityContext = {};
+            const exp = reader.expectOption("p", "a", "r", "s", "e");
+            if (!helper.merge(exp)) {
+                return helper.fail();
+            }
+            switch (exp.data) {
+                case "p":
+                    context.limit = 1;
+                    context.type = ["minecraft:player"];
+                    break;
+                case "a":
+                    context.type = ["minecraft:player"];
+                    break;
+                case "r":
+                    context.limit = 1;
+                    context.type = ["minecraft:player"];
+                    break;
+                case "s":
+                    context.limit = 1;
+                    break;
+                case "e":
+                    break;
+                default:
+                    throw new TypeError();
+            }
+            if (reader.canRead() && reader.peek() === "[") {
+                let next = reader.read();
+                while (next !== "]") {
+                    const arg = reader.readUnquotedString();
+                    if (!helper.merge(reader.expect("="))) {
+                        return helper.fail();
+                    }
+                    const opt = options[arg];
+
+                    const conc = opt.parse(reader, info, context);
+                    if (!helper.merge(conc)) {
+                        return helper.fail();
+                    }
+                    if (conc.data) {
+                        Object.assign(context, conc.data);
+                    }
+                    const nextc = reader.expectOption(",", "]");
+                    if (!helper.merge(nextc)) {
+                        return helper.fail();
+                    }
+                    next = nextc.data;
+                }
+            }
+            const conterr = getContextError(
+                context,
+                info.node_properties as NodeProp
+            );
+            if (conterr) {
+                return helper.fail(conterr.create(start, reader.cursor));
+            }
+            return helper.succeed();
+        } else if (uuidregex.test(reader.getRemaining().substr(0, 36))) {
+            helper.addErrors(
+                uuidwarn.create(reader.cursor, reader.cursor + 36)
+            );
+            reader.cursor += 36;
+            /*
+            const conterr = getContextError(
+                {
+                    ...info.context,
+                    limit: 1
+                },
+                info.node_properties as NodeProp
+            );
+            if (conterr) {
+                return helper.fail(
+                    conterr.create(reader.cursor - 36, reader.cursor)
+                );
+            }
+            */
+            return helper.succeed();
+        } else if (this.fakePlayer) {
+            if (info.data.localData && info.data.localData.nbt.scoreboard) {
+                for (const score of info.data.localData.nbt.scoreboard.data
+                    .PlayerScores) {
+                    if (reader.getRemaining().startsWith(score.Name)) {
+                        helper.addSuggestion(reader.cursor, score.Name);
+                    }
+                }
+            }
+            reader.readWhileRegexp(/[^\s]/);
+            return helper.succeed();
+        }
+        const name = reader.readUnquotedString();
+        if (name === "") {
+            return helper.fail();
+        }
+        return helper.succeed({
+            entity: "minecraft:player",
+            isSingle: true
+        } as ContextChange);
+    }
+}
